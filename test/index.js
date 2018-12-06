627010c110b7efa28b12a3961bd0368bbb947eb4
廖伟 <liaowei@liaoweideMacBook-Pro.local>
2018-12-06 18:00:18 +0800

    代码更新

--- test/index.js
+++ test/index.js
@@ -1,18 +1,43 @@
-const Linter = require("eslint").Linter;
-const linter = new Linter();
 
+var RuleTester = require("eslint").RuleTester;
+
+
+var ruleTester = new RuleTester();
 const rule = require('../index')
 
 
-linter.defineRule("require", rule.rules.require);
-
-const results = linter.verify(`
-  var a = require('./a.js')
-`, { 
-  rules: { 
-    "require": "error" 
-  } 
+ruleTester.run("require", rule.rules.require, {
+    valid: [
+        { code: "require('./a')" }
+    ],
+    invalid: [
+        {
+            code: "require('a')",
+            errors: 1
+        }
+    ]
 });
 
 
-console.log(results)
+
+ruleTester.run("page-null-callback", rule.rules['page-null-callback'], {
+    valid: [
+        { code: "class A { onCache() {this} }", parserOptions: { ecmaVersion: 6 } }
+    ],
+    invalid: [
+        {
+            code: `
+                class Page {
+                    onCreate () {
+                      // abc注释
+                    }
+                    onCache () {
+
+                    }
+                }
+            `,
+            parserOptions: { ecmaVersion: 6 },
+            errors: 2
+        }
+    ]
+});
